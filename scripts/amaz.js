f870b6e9792877cde4c43046f244a5480df5eb92
OneScriptMan <vladzam79@gmail.com>
2023-10-01 16:55:55 +0300

    Make the cart quantity interactive

--- scripts/amaz.js
+++ scripts/amaz.js
@@ -124,7 +124,13 @@ const products = [
         });
       };
 
-      
-      console.log(cart);
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
     });
   })
