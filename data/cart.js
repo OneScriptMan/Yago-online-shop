f11127784b98e60c288bbecf3d17c6ebadc87c50
OneScriptMan <vladzam79@gmail.com>
2023-10-07 14:29:49 +0300

    add save button

--- data/cart.js
+++ data/cart.js
@@ -4,20 +4,10 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart) {
 
-  cart = [
-    {
-      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2,
-    },
-    {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1
-    }
-  ];
 }
 
 
-function saveToStorage() {
+export function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify (cart));
 }
 
@@ -64,6 +54,5 @@ export function calculateCartQuantity(){
   cart.forEach((cartItem) => {
     cartQuantity += cartItem.quantity;
   });
-  console.log(cartQuantity);
   return cartQuantity;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,40 @@
-import {cart, removeFromCart,calculateCartQuantity} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+
+function updateCartQuantity() {
+  document.querySelector('.js-checkout-quantity').innerHTML = calculateCartQuantity() + ' items';
+}
+
+function clickUpdate(productId){
+  document.querySelector(`.js-save-quantity-link-${productId}`).classList.remove('is-editing-quantity');
+
+  document.querySelector(`.js-quantity-input-${productId}`).classList.remove('is-editing-quantity');
+}
+
+function saveUpdate(productId){
+  document.querySelector(`.js-save-quantity-link-${productId}`).addEventListener('click', () => {
+    document.querySelector(`.js-save-quantity-link-${productId}`).classList.add('is-editing-quantity');
+    document.querySelector(`.js-quantity-input-${productId}`).classList.add('is-editing-quantity');
+
+    let selectValue = 0;
+
+    selectValue = Number(document.querySelector(`.js-quantity-input-${productId}`).value);
+
+    addToCart(productId, selectValue);
+
+    console.log(selectValue);
+
+    cart.forEach((cartItem)=>{
+      if (cartItem.productId === productId) {
+        document.querySelector(`.js-quantity-label-${productId}`).innerHTML = cartItem.quantity;
+      }
+    });//render quanity
+
+    updateCartQuantity();
+
+  });
+}
 
 let cartSummaryHTML = '';
 
@@ -37,19 +71,28 @@ cart.forEach((cartItem) => {
         </div> 
         <div class="product-quantity">
           <span>
-            Quantity: <span class="quantity-label">
+            Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">
             ${cartItem.quantity}
             </span>
           </span>
-          <span class="update-quantity-link link-primary">
+
+          <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
             Update
           </span>
+
           <span class="delete-quantity-link link-primary js-delete-link"
           data-product-id="${matchingProduct.id}"
           >
             Delete
           </span>
+
         </div>
+  
+        <input type="number" min="0" value="${1}" class="quantity-input js-quantity-input-${matchingProduct.id} is-editing-quantity">
+
+        <span class="save-quantity-link js-save-quantity-link js-save-quantity-link-${matchingProduct.id}
+        link-primary is-editing-quantity" data-product-id = "${matchingProduct.id}">Save</span>
+
       </div>
       <div class="delivery-options">
       <div class="delivery-options-title">
@@ -89,19 +132,56 @@ cart.forEach((cartItem) => {
           <div class="delivery-option-date">
             Monday, June 13
           </div>
-          <div class="delivery-option-price">
-            $9.99 - Shipping
+            <div class="delivery-option-price">
+              $9.99 - Shipping
+            </div>
           </div>
         </div>
       </div>
-      </div>
-      </div>
-      </div>
+    </div>
+  </div>
   `;
 
 });
 
+
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-update-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    let productId = link.dataset.productId;
+    
+    //clickUpdate(productId);
+    document.querySelector(`.js-save-quantity-link-${productId}`).classList.remove('is-editing-quantity');
+
+    document.querySelector(`.js-quantity-input-${productId}`).classList.remove('is-editing-quantity');  
+
+    //saveUpdate(productId);
+      
+  });
+});
+
+document.querySelectorAll('.js-save-quantity-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    let productId = link.dataset.productId;
+    document.querySelector(`.js-save-quantity-link-${productId}`).classList.add('is-editing-quantity');
+    document.querySelector(`.js-quantity-input-${productId}`).classList.add('is-editing-quantity');
+
+    let selectValue = 0;
+    selectValue = Number(document.querySelector(`.js-quantity-input-${productId}`).value);
+
+    addToCart(productId, selectValue);
+
+    cart.forEach((cartItem)=>{
+      if (cartItem.productId === productId) {
+        document.querySelector(`.js-quantity-label-${productId}`).innerHTML = cartItem.quantity;
+      }
+    });//render quanity
+  
+    updateCartQuantity();
+  })
+})
+
 
 document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
@@ -113,15 +193,9 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     updateCartQuantity();
 
     container.remove();
- 
     
   })// команда link.dataset.data "преобразовывает" 'data-product-id' в 'productId'
   
 });
-
-function updateCartQuantity() {
-
-  document.querySelector('.js-checkout-quantity').innerHTML = calculateCartQuantity() + ' items';
-}
 
 updateCartQuantity();
